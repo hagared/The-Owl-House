0168f0b8a927051aa84627e3f1aebabff56bd72c
hagared <157571237+hagared@users.noreply.github.com>
2026-01-01 15:22:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,111 +1,106 @@
-/* ====== ДАННЫЕ СЕЗОНОВ ====== */
 const seasons = {
   s1: {
     title: "1 сезон",
     episodes: [
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239038&hash=62c3faaa88d74a78&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239019&hash=3c414f1d2f4f8b50&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239020&hash=bb094a37f531b670&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239021&hash=09a03fa28cc27fd1&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239022&hash=055b7991a53a700c&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239040&hash=99230916bebf970e&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239024&hash=8e45f419ca56617f&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239025&hash=7d6bc6b9134cd447&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239026&hash=ba3ce4dba4db1d47&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239027&hash=e20e07778c6bbe11&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239028&hash=abfc3e14328c791e&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239029&hash=8bc87ea0d8d91b38&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239041&hash=67d10abcf86e1b6a&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239042&hash=7b4f46262c48a365&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239032&hash=f6f8baed9362bb47&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239033&hash=28138c13be86590f&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239034&hash=b688adb1382d3879&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239043&hash=c704e56e6ecc00c8&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239017&hash=9e866861d1aa2112&hd=3" }
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239038&hash=62c3faaa88d74a78&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239019&hash=3c414f1d2f4f8b50&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239020&hash=bb094a37f531b670&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239021&hash=09a03fa28cc27fd1&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239022&hash=055b7991a53a700c&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239040&hash=99230916bebf970e&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239024&hash=8e45f419ca56617f&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239025&hash=7d6bc6b9134cd447&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239026&hash=ba3ce4dba4db1d47&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239027&hash=e20e07778c6bbe11&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239028&hash=abfc3e14328c791e&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239029&hash=8bc87ea0d8d91b38&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239041&hash=67d10abcf86e1b6a&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239042&hash=7b4f46262c48a365&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239032&hash=f6f8baed9362bb47&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239033&hash=28138c13be86590f&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239034&hash=b688adb1382d3879&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239043&hash=c704e56e6ecc00c8&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239017&hash=9e866861d1aa2112&hd=3" }
     ]
   },
 
   s2p1: {
     title: "2 сезон — 1 часть",
     episodes: [
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239044&hash=9d04af8ae1b8326b&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239045&hash=455984dd077d45c2&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239046&hash=ccca72761b3819be&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239057&hash=6da5426b9fa75d7d&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239047&hash=8ae70aa2608206b6&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239048&hash=4a9e1217348a0c28&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239058&hash=e8e72bb1c54af558&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239049&hash=55fb7109831f52c1&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239050&hash=9ef6405a81fc4c68&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239051&hash=1a841fa84f6e6a41&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239052&hash=9b719e4ce6b5ebe6&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239059&hash=0d2c915772576cbb&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239053&hash=5849ea15bc531272&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239054&hash=f628d63d9aaf79f1&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239055&hash=412a25c22c307e44&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239056&hash=44c0e177f34f734f&hd=3" }
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239044&hash=9d04af8ae1b8326b&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239045&hash=455984dd077d45c2&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239046&hash=ccca72761b3819be&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239057&hash=6da5426b9fa75d7d&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239047&hash=8ae70aa2608206b6&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239048&hash=4a9e1217348a0c28&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239058&hash=e8e72bb1c54af558&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239049&hash=55fb7109831f52c1&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239050&hash=9ef6405a81fc4c68&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239051&hash=1a841fa84f6e6a41&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239052&hash=9b719e4ce6b5ebe6&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239059&hash=0d2c915772576cbb&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239053&hash=5849ea15bc531272&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239054&hash=f628d63d9aaf79f1&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239055&hash=412a25c22c307e44&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239056&hash=44c0e177f34f734f&hd=3" }
     ]
   },
 
   s2p2: {
     title: "2 сезон — 2 часть",
     episodes: [
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239060&hash=bf85746339b8d0ce&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239061&hash=da94d69943ebe932&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239062&hash=f1913b15760f57ef&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239064&hash=f143337caaa9c4a6&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239063&hash=09fc3067db617364&hd=3" }
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239060&hash=bf85746339b8d0ce&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239061&hash=da94d69943ebe932&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239062&hash=f1913b15760f57ef&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239064&hash=f143337caaa9c4a6&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239063&hash=09fc3067db617364&hd=3" }
     ]
   },
 
   s3: {
     title: "3 сезон",
     episodes: [
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239065&hash=89dcb085148d4f8b&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239066&hash=ef1eafad0b88832f&hd=3" },
-      { type: "vk", url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239067&hash=760438a5d65c5210&hd=3" }
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239065&hash=89dcb085148d4f8b&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239066&hash=ef1eafad0b88832f&hd=3" },
+      { url: "https://vkvideo.ru/video_ext.php?oid=-229058224&id=456239067&hash=760438a5d65c5210&hd=3" }
     ]
   }
 };
 
-/* ====== НАВИГАЦИЯ ====== */
 function goToSeasons() {
-  document.getElementById("step-1").classList.add("hidden");
-  document.getElementById("step-2").classList.remove("hidden");
+  step("step-1", "step-2");
 }
 
 function backToSeasons() {
-  document.getElementById("step-3").classList.add("hidden");
-  document.getElementById("step-2").classList.remove("hidden");
-  document.getElementById("videoFrame").src = "";
+  step("step-3", "step-2");
+  videoFrame.src = "";
+}
+
+function step(hide, show) {
+  document.getElementById(hide).classList.add("hidden");
+  document.getElementById(show).classList.remove("hidden");
 }
 
 function openSeason(key) {
   const season = seasons[key];
   if (!season) return;
 
-  document.getElementById("step-2").classList.add("hidden");
-  document.getElementById("step-3").classList.remove("hidden");
-
+  step("step-2", "step-3");
   document.querySelector("#step-3 h2").textContent = season.title;
 
-  const episodesDiv = document.getElementById("episodes");
-  const player = document.getElementById("player");
-  const frame = document.getElementById("videoFrame");
-
-  episodesDiv.innerHTML = "";
-  frame.src = "";
+  episodes.innerHTML = "";
+  videoFrame.src = "";
   player.classList.add("hidden");
 
-  season.episodes.forEach((ep, index) => {
+  season.episodes.forEach((ep, i) => {
     const btn = document.createElement("button");
     btn.className = "btn outline";
-    btn.textContent = `${index + 1} серия`;
+    btn.textContent = `${i + 1} серия`;
     btn.onclick = () => {
-      frame.src = ep.url;
+      videoFrame.src = ep.url;
       player.classList.remove("hidden");
-      frame.scrollIntoView({ behavior: "smooth" });
+      videoFrame.scrollIntoView({ behavior: "smooth" });
     };
-    episodesDiv.appendChild(btn);
+    episodes.appendChild(btn);
   });
 }
